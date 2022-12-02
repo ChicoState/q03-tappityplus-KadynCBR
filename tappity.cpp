2ad64d6c115a42f5f5a48da0c2c056097bb1320c
KadynCBR <kadyn_martinez@hotmail.com>
2022-12-01 18:17:38 -0800

    Added Testing for tappity

--- tappity.cpp
+++ tappity.cpp
@@ -1,40 +1,49 @@
 #include "tappity.h"
 /**
  * Class for tappity
-**/
+ **/
 
-//Constructor sets the reference phrase
-tappity::tappity(std::string reference)
-{
+// Constructor sets the reference phrase
+tappity::tappity(std::string reference) {
+  reference_ = reference;
+  input_ = "";
 }
 
-//Provide the input to be compared to the reference. Before this 
-//function is called, the input should be considered an empty string
-void tappity::entry(std::string input)
-{
-}
+// Provide the input to be compared to the reference. Before this
+// function is called, the input should be considered an empty string
+void tappity::entry(std::string input) { input_ = input; }
 
-//Compares the length of the reference to that of the input and
-//returns the absolute value of the difference, or 0 if they are the
-//same length
-int tappity::length_difference()
-{
-  return 0;
-}
+// Compares the length of the reference to that of the input and
+// returns the absolute value of the difference, or 0 if they are the
+// same length
+int tappity::length_difference() { return abs(input_.length() - reference_.length()); }
 
-//Compares the content of the reference to that of the input and
-//calculates the accuracy of matching characters. If the two strings
-//are identical, accuracy should be 100. However, for each 
-//corresponding character that does not match in the same location,
-//the percentage of corresponding characters should be returned.
-//For example, if the reference and input have 10 letters and the input
-//matches 8 of the corresponding reference characters, the function 
-//should return 80. When input does not have the same number of
-//characters as the reference, the accuracy should represent the percent
-//of matching characters between the shorter and longer strings. For
-//example, if one string has 8 characters matching the corresponding
-//locations in another string that has 16 characters, the accuracy is 50.
-double tappity::accuracy()
-{
-  return 0;
+// Compares the content of the reference to that of the input and
+// calculates the accuracy of matching characters. If the two strings
+// are identical, accuracy should be 100. However, for each
+// corresponding character that does not match in the same location,
+// the percentage of corresponding characters should be returned.
+// For example, if the reference and input have 10 letters and the input
+// matches 8 of the corresponding reference characters, the function
+// should return 80. When input does not have the same number of
+// characters as the reference, the accuracy should represent the percent
+// of matching characters between the shorter and longer strings. For
+// example, if one string has 8 characters matching the corresponding
+// locations in another string that has 16 characters, the accuracy is 50.
+double tappity::accuracy() {
+  int max_length = std::max(reference_.length(), input_.length());
+  int min_length = std::min(reference_.length(), input_.length());
+  if (max_length == 0) {
+    return 100.0;
+  }
+
+  int matches = 0;
+
+  for (int i = 0; i < min_length; i++) {
+    if (reference_[i] == input_[i]) {
+      matches++;
+    }
+  }
+  double accuracy = (matches / double(max_length)) * 100.0;
+  return accuracy;
 }
--- tappityTest.cpp
+++ tappityTest.cpp
@@ -1,15 +1,84 @@
 /**
  * Unit Test suite
-**/
+ **/
 
 #include <gtest/gtest.h>
+
 #include "tappity.h"
- 
-class tappityTest : public ::testing::Test
-{
-	protected:
-		tappityTest(){} 
-		virtual ~tappityTest(){} 
-		virtual void SetUp(){}
-		virtual void TearDown(){}
+
+class tappityTest : public ::testing::Test {
+ protected:
+  tappityTest() {}
+  virtual ~tappityTest() {}
+  virtual void SetUp() {}
+  virtual void TearDown() {}
 };
+
+TEST(tappityTest, Construction_with_empty) {
+  tappity t = tappity("");
+  ASSERT_TRUE(true);
+}
+
+TEST(tappityTest, length_difference_empty_both) {
+  tappity t = tappity("");
+  ASSERT_EQ(t.length_difference(), 0);
+}
+
+TEST(tappityTest, length_difference_empty_entry) {
+  tappity t = tappity("foobar");
+  ASSERT_EQ(t.length_difference(), 6);
+}
+
+TEST(tappityTest, length_difference_empty_reference) {
+  tappity t = tappity("");
+  t.entry("foobar");
+  ASSERT_EQ(t.length_difference(), 6);
+}
+
+TEST(tappityTest, length_difference_same) {
+  tappity t = tappity("foobar");
+  t.entry("foobar");
+  ASSERT_EQ(t.length_difference(), 0);
+}
+
+TEST(tappityTest, accuracy_empties) {
+  tappity t = tappity("");
+  t.entry("");
+  ASSERT_DOUBLE_EQ(t.accuracy(), 100);
+}
+
+TEST(tappityTest, accuracy_reference_longer) {
+  tappity t = tappity("reference1");
+  t.entry("refer");
+  ASSERT_DOUBLE_EQ(t.accuracy(), 50);
+}
+
+TEST(tappityTest, accuracy_input_longer) {
+  tappity t = tappity("refer");
+  t.entry("reference1");
+  ASSERT_DOUBLE_EQ(t.accuracy(), 50);
+}
+
+TEST(tappityTest, accuracy_same) {
+  tappity t = tappity("reference");
+  t.entry("reference");
+  ASSERT_DOUBLE_EQ(t.accuracy(), 100);
+}
+
+TEST(tappityTest, accuracy_reference_half_accuracy) {
+  tappity t = tappity("0123456789");
+  t.entry("012");
+  ASSERT_DOUBLE_EQ(30, t.accuracy());
+}
+
+TEST(tappityTest, accuracy_spotted_correct) {
+  tappity t = tappity("abcdefghij");
+  t.entry("a0c0e0g0i0");
+  ASSERT_DOUBLE_EQ(50, t.accuracy());
+}
+
+TEST(tappityTest, accuracy_spotted_correct_inverse) {
+  tappity t = tappity("a0c0e0g0i0");
+  t.entry("abcdefghij");
+  ASSERT_DOUBLE_EQ(50, t.accuracy());
+}
